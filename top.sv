f153b6875b8f0a06318f71231c1e8c27e39c52ad
Tian Xie <145227971+tixie2027@users.noreply.github.com>
2025-09-10 12:02:49 -0700

    Modify top module to add led outputs and simplify code

--- top.sv
+++ top.sv
@@ -1,36 +1,13 @@
 module top (input logic [3:0] s1,
                   input logic [3:0] s2,
-                  input logic led1, led2,
-                  output logic [4:0] sum
+                  output logic led1, led2,
+                  output logic [4:0] sum,
                   output logic [6:0] seg);
-
-    logic int_osc;
-	logic toggle = 0;
-	logic [24:0] counter = 0;
+				  
+	logic int_osc;
 	
 	HSOSC hf_osc (.CLKHFPU(1'b1), .CLKHFEN(1'b1), .CLKHF(int_osc));
-
-    always_ff @(posedge int_osc)
-		begin
-            if (counter == 18'd200000) begin
-                counter <= 0;
-                toggle <= ~toggle;
-            end else begin 
-			    counter <= counter + 1;
-		    end
-        end
-
-    if (toggle == 0)
-        s = s1;
-    else
-        s = s2;
-
-    assign led1 = toggle;
-    assign led2 = ~toggle;
-	
-	// calls 7 segments
-    segment7 s7 (s, seg);
-    ledsum ls (s1, s2, sum);
+	digitdisplay display (s1, s2, int_osc, led1, led2, sum, seg);
 
 endmodule
 
